a776485e27e0c965311be99d547ece3f3f918a8c
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2021-08-27 16:07:57 +0300

    created functionality for work elements

--- src/js/lib/modules/actions.js
+++ src/js/lib/modules/actions.js
@@ -1,36 +1,71 @@
 import $ from '../core';
 
-$.prototype.on = function(eventName, callback) {
-    if (!eventName || !callback) {
-        return this;
+$.prototype.html = function(content) {
+    for (let i = 0; i < this.length; i++) {
+       if (content) {
+           this[i].innerHTML = content;
+       } else {
+            return this[i].innerHTML;
+       }
     }
 
-    for(let i = 0; i < this.length; i++) {
-        this[i].addEventListener(eventName, callback);
-    }
     return this;
 };
 
-$.prototype.off = function(eventName, callback) {
-    if (!eventName || !callback) {
-        return this;
+
+$.prototype.eq = function(i) {
+    const swap = this[i];
+    const objLength = Object.keys(this).length;
+
+    for (let i = 0; i < objLength; i++) {
+        delete this[i];
     }
 
-    for(let i = 0; i < this.length; i++) {
-        this[i].removeEventListener(eventName, callback);
-    }
+    this[0] = swap;
+    this.length = 1;
+
     return this;
 };
 
-$.prototype.click = function(handler) {
-    for(let i = 0; i < this.length; i++) {
-        if (handler) {
-            this[i].addEventListener('click', handler);
-        } else {
-            this[i].click();
+
+$.prototype.index = function() {
+    const parent = this[0].parentNode;
+    const childs = [...parent.children];
+
+    const findMyIndex = (item) => {
+        return item == this[0];
+    }
+
+    return childs.findIndex(findMyIndex);
+};
+
+
+
+$.prototype.find = function(selector) {
+    let numberOfItems = 0;
+    let counter = 0;
+
+    const copyObj = Object.assign({}, this);
+
+    for (let i = 0; i < copyObj.length; i++) {
+        const arr = copyObj[i].querySelectorAll(selector);
+        if (arr.length == 0) {
+            continue;
         }
+
+        for (let j = 0; j < arr.length; j++) {
+            this[counter] = arr[j];
+            counter++;
+        }
+
+        numberOfItems += arr.length;
     }
+
+    this.length = numberOfItems;
+    const objLength = Object.keys(this).length;
+    for(; numberOfItems < objLength; numberOfItems++) {
+        delete this[numberOfItems];
+    }
+
     return this;
 };
-
-
--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,25 @@
 import $ from './lib/lib';
 
 $('button').on('click', function() {
-    $(this).hide().show().toggleClass('active');
+    $('div').eq(2).toggleClass('active');
 });
 
 const type = $('button').getAttr('type');
-console.log(type); //значение атрибута в консоль;
+// console.log(type); //значение атрибута в консоль;
 
 $('button').setAttr('id', 'button'); //установка элементу нового id;
 
 $('div').removeAttr('style'); //удаление атрибута у элемента
 
 $('button').toggleAttr('dir', 'rtl'); //карусель атрибута
+
+
+// console.log($('button').html('Hello'));
+
+
+$('div').click(function() {
+    console.log($(this).index());
+});
+
+console.log($('div').eq(2).find('.some'));
+
